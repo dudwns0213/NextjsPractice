50ce0e85a3ee0d7dc0d8a8a474e165f08580d10a
dudwns0213 <dudwns0213@gmail.com>
2024-05-30 03:13:00 +0900

    Nextjs 실습 커밋

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
+import Container from "@/components/Container";
+import Header from "@/components/Header";
 import "@/styles/globals.css";
+import { ThemeProvider } from "@/lib/ThemeContext";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Wa</title>
+      </Head>
+      <ThemeProvider>
+        <Header />
+        <Container page>
+          <Component {...pageProps} />
+        </Container>
+      </ThemeProvider>
+    </>
+  );
 }
